bbb176f97368b5b2f34abd53db6f8b77e0980eb4
Sitnikov <selnetys@gmail.com>
2022-06-03 22:04:40 +0500

    added menu

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const navOffcanvas = document.getElementById('offcanvasNavbar')
+const bsOffcanvas = new bootstrap.Offcanvas(navOffcanvas)
 
+const links = document.querySelectorAll('.nav-link')
+
+const closeOffcanvas = () => {
+  bsOffcanvas.hide()
+}
+
+links.forEach((link) => {
+  link.addEventListener('click', (ev) => {
+    closeOffcanvas()
+    // document.getElementById('myId').scrollIntoView({ behavior: 'smooth' })
+  })
+})
