8aaf8c3b49af769dac314afca981b84dec760b90
Sitnikov <selnetys@gmail.ru>
2022-06-16 17:51:13 +0500

    compl filter

--- main.js
+++ main.js
@@ -13,3 +13,34 @@ links.forEach((link) => {
     // document.getElementById('myId').scrollIntoView({ behavior: 'smooth' })
   })
 })
+
+const dropdown = document.querySelector('.filter_dropdown')
+const subMenus = dropdown.querySelectorAll('.filter-menu-submenu')
+const button = dropdown.querySelector('.filter_button')
+
+button.addEventListener('click', () => {
+  dropdown.classList.toggle('opened')
+})
+subMenus.forEach((submenu) => {
+  submenu.querySelector('label').addEventListener('click', () => {
+    submenu.classList.toggle('opened')
+  })
+})
+
+const dropdownMini = document.querySelector('.dropdown-mini')
+dropdownMini
+  .querySelector('.dropdown-mini-name')
+  .addEventListener('click', () => {
+    dropdownMini.classList.toggle('opened')
+  })
+
+document.addEventListener('click', (e) => {
+  const withinBoundaries = e.composedPath().some((el) => {
+    return [dropdown, dropdownMini].includes(el)
+  })
+
+  if (!withinBoundaries) {
+    dropdown.classList.remove('opened')
+    dropdownMini.classList.remove('opened')
+  }
+})
